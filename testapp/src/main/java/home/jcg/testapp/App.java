6468919de3817fa038dc5ce83e8b502d9e11e621
julio49 <julio49@verizonn.net>
2017-01-14 12:37:28 -0500

    added function getNextDate

--- testapp/src/main/java/home/jcg/testapp/App.java
+++ testapp/src/main/java/home/jcg/testapp/App.java
@@ -1,5 +1,8 @@
 package home.jcg.testapp;
 
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,32 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        new App();
     }
+    
+
+	public App()
+    {
+        System.out.println( "Hello World!" + getNextDate( new Date()).getTime());
+    }
+    
+    
+    private Calendar getNextDate(Date date)
+    {
+    	Date nextDate = addDays(date, 1);
+    	Calendar calendar = Calendar.getInstance();
+        calendar.setTime(nextDate);
+    	
+    	return calendar;
+    }
+    
+    
+    public static Date addDays(Date baseDate, int daysToAdd) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(baseDate);
+        calendar.add(Calendar.DAY_OF_YEAR, daysToAdd);
+        return calendar.getTime();
+    }
+    
+    
 }
